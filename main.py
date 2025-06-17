0c3d00b22bfcff1f8d33fefce5dc8615d420a7f4
Matt <testuserathome@gmail.com>
2025-06-17 10:50:45 -0400

    Complete bookbot v1.0

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 from stats import get_num_words
+from stats import count_char
+from stats import sort_dict
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -6,9 +9,27 @@ def get_book_text(filepath):
     return file_contents
 
 def main():
-    filepath = 'books/frankenstein.txt'
-    book_text = get_book_text(filepath)
+    if len(sys.argv) !=2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    filepath = sys.argv[1]
+    #Count number of words in text
+    try:
+        book_text = get_book_text(filepath)
+    except FileNotFoundError:
+        print("Invalid path to tile.")
+        sys.exit(1)
     word_count = get_num_words(book_text)
-    print(f"{word_count} words found in the document")
-
+    print("============ BOOKBOT ============")
+    print("Analyzing words found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    #Count number of each character type
+    char_dict = count_char(book_text)
+    #Return sorted character count list from greatest to least frequemt
+    char_list = sort_dict(char_dict)
+    print("--------- Character Count -------")
+    for item in char_list:
+        print(f"{item['char']}: {item['count']}")
+    print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -1,4 +1,26 @@
+def sort_on(dict):
+    return dict["count"]
+
+def sort_dict(dict):
+    char_list = []
+    for key, value in dict.items():
+        new_dict = {'char' : key, 'count' : value}
+        char_list.append(new_dict)
+    char_list.sort(reverse=True, key=sort_on)
+    return char_list
+    
+
 def get_num_words(book_text):
     word_list = book_text.split()
     word_count = len(word_list)
     return word_count
+
+def count_char(book_text):
+    book_text = book_text.replace('\n', ' ') #Replace '\n' with spaces
+    char_dict = {}
+    for char in book_text:
+        if char.lower() in char_dict:
+            char_dict[char.lower()] += 1
+        else:
+            char_dict[char.lower()] = 1
+    return char_dict
